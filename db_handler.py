4745fb9ee9abd4d99b719dece3903b942c1c57b2
Fenikonir <belovemil@gmail.com>
2020-11-06 20:53:53 +0300

    Продолжение продолжения работы над проектом

---
+++ db_handler.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+
+# Добавление, Проверка и Авторизация
+def login(login, password, signal):
+    con = sqlite3.connect("handler/users.db")
+    cur = con.cursor()
+
+    result = cur.execute("""SELECT * FROM Users WHERE login=?""", (login,)).fetchall()
+
+    if result != [] and result[0][2] == password:
+
+        signal.emit("Авторизация прошла успешно!")
+
+    else:
+        signal.emit("Неверный логин или пароль!")
+
+    cur.close()
+    con.close()
+
+
+def register(login, password, signal):
+    con = sqlite3.connect("handler/users.db")
+    cur = con.cursor()
+    cur.execute("""SELECT * FROM Users WHERE login=?""", (login,))
+    log_table = cur.fetchall()
+    if log_table != []:
+        signal.emit("Пользователь уже зарегестрирован")
+    elif log_table == []:
+        cur.execute("""INSERT INTO Users(login, password) VALUES(?,?)""", (login, password)).fetchall()
+        signal.emit("Пользователь зарегестрирован")
+        con.commit()
+
+    cur.close()
+    con.commit()
+
+
+def admin_root(user):
+    con = sqlite3.connect("handler/users.db")
+    cur = con.cursor()
+
+    result = cur.execute("""SELECT admin_root FROM Users WHERE login=?""", (user,)).fetchall()
+    cur.close()
+    con.commit()
+    return result[0][0]
